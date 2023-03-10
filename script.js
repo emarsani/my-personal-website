5c66b5f32bd5ea336ff8474c10c6e777ac2769c9
moti <belinamoti@email.com>
2023-03-10 12:05:40 -0500

    Added script

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 
 
-// const h2 = document.createElement("h2");
-// h2.textContent = "This content added by JavaScript";
+const button = document.getElementById("button");
 
-// document.querySelector("body").appendChild(h2);
+function myFunction() {
+    const contactDetails = document.getElementById("contact-details");
+    contactDetails.style.textAlign = "center";
+    contactDetails.style.color = "#191825"
+    contactDetails.style.fontSize = "14px";
+    contactDetails.innerHTML = "<h1>Open hours:</h1><h2>999-999-9999</h2></h2><h3>Monday from 9am - 5pm</h3><h3>Tuesday from 9am - 5pm</h3><h3>Wednesday from 9am - 5pm</h3><h3>Thursday from 9am - 5pm</h3><h3>Friday from 9am - 4pm</h3>";
+}
+
+button.addEventListener("click", myFunction);
